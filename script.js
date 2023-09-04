4638200c09df0103301616cb104bf7d3ca0a5cad
nicososa01 <nicolassosacarsin@gmail.com>
2023-09-04 20:02:55 -0300

    Correcciones
    Correcciones de js y html

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 var http = new XMLHttpRequest();
 var url = "https://jsonplaceholder.typicode.com/users";
-var nombre = document.getElementById('name');
-var lastName = document.getElementById('lastName');
-var nacimiento = document.getElementById('nacimiento');
-http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+var nombre = document.getElementById('name').value;
+var apellido = document.getElementById('lastName').value;
+var nacimiento = document.getElementById('born').value;
+
 http.open("POST", url, true);
+http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
 http.onreadystatechange = function() {
-    if(http.readyState == 4 && http.status == 200) { 
-       //aqui obtienes la respuesta de tu peticion
-       alert(http.responseText);
+    if (http.readyState == 4) { 
+        if (http.status == 200) {
+            // Aquí obtienes la respuesta de tu petición
+            alert(http.responseText);
+        } else {
+            
+            alert("Error en la solicitud: " + http.status);
+        }
     }
 }
-http.send(JSON.stringify({name:nombre, lastName: lastName, nacimiento: nacimiento}));
+
+var data = {
+    name: nombre,
+    lastName: apellido,
+    nacimiento: nacimiento
+};
+
+http.send(JSON.stringify(data));
