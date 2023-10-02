337a11dc87888951dcb0b56c9b853fd69f2459d6
nicososa01 <nicolassosacarsin@gmail.com>
2023-10-01 21:48:41 -0300

    Intento 2

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-var http = new XMLHttpRequest();
-var url = "https://jsonplaceholder.typicode.com/users";
+document.addEventListener("DOMContentLoaded", function() {
+    var http = new XMLHttpRequest();
+    var url = "https://jsonplaceholder.typicode.com/users";
+    var sendButton = document.getElementById('send');
+    var responseElement = document.getElementById('response');
 
-http.open("POST", url, true);
-http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    sendButton.addEventListener('click', function() {
+        var nombre = document.getElementById('name').value;
+        var apellido = document.getElementById('lastName').value;
+        var nacimiento = document.getElementById('born').value;
 
-document.getElementById('send').addEventListener('click', function() {
-    var nombre = document.getElementById('name').value;
-    var apellido = document.getElementById('lastName').value;
-    var nacimiento = document.getElementById('born').value;
+        var data = {
+            name: nombre,
+            lastName: apellido,
+            nacimiento: nacimiento
+        };
 
-    var data = {
-        name: nombre,
-        lastName: apellido,
-        nacimiento: nacimiento
-    };
+        http.open("POST", url, true);
+        http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-    http.onreadystatechange = function() {
-        if (http.readyState == 4) { 
-            if (http.status == 200) {
-                // Aquí obtienes la respuesta de tu petición
-                alert(http.responseText);
-            } else {
-                alert("Error en la solicitud: " + http.status);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4) { 
+                if (http.status == 200) {
+                    
+                    responseElement.innerHTML = "Respuesta del servidor: " + http.responseText;
+                } else {
+                    responseElement.innerHTML = "Error en la solicitud: " + http.status;
+                }
             }
-        }
-    };
+        };
 
-    http.send(JSON.stringify(data));
+        http.send(JSON.stringify(data));
+    });
 });
